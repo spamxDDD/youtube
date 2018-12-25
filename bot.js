78b6eaee584bb6e54c913c95064ddef2c7a55ac6
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-25 14:22:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -239,22 +239,6 @@ client.on("message", (message) => {
     }
  
 });
- client.on('guildMemberRemove', member => {
-        var embed = new Discord.RichEmbed()
-        .setAuthor(member.user.username, member.user.avatarURL)
-        .setThumbnail(member.user.avatarURL)
-        .setTitle(`بس بعرف وين رحت؟؟؟ :raised_hand::skin-tone-1: :pensive:`)
-        .setDescription(`مع السلامه تشرفنا بك :raised_hand::skin-tone-1: :pensive: `)
-        .addField('   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-        .setColor('PURPLE')
-        .setFooter(`====اهلا السيرفر نور بيك و الله====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
-   
-    var channel =member.guild.channels.find('name', 'welcome-‹‹')
-    if (!channel) return;
-    channel.send({embed : embed});
- 
-    });
-
 
 
 
