6efd379b3bb95f5e6b4e9867b51e042a3b60d1de
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-25 13:53:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -155,6 +155,13 @@ client.on('guildMemberRemove', member => {
  
 });
 
+    client.on("guildMemberAdd", member => {
+    member.createDM().then(function (channel) {
+        return channel.send(`:rose:  ولكم نورت السيرفر :rose:
+        :crown: اسم العضو  ${member}:crown:  
+        انت العضو رقم ${member.guild.memberCount} `)
+    }).catch(console.error)
+})
 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
