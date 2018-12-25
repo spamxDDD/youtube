74936b14dedd65ca448115493f0ff3557ff826a6
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-25 13:56:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -162,6 +162,34 @@ client.on('guildMemberRemove', member => {
         انت العضو رقم ${member.guild.memberCount} `)
     }).catch(console.error)
 })
+ if(msg.content.startsWith ('$server')) {
+    let embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setThumbnail(msg.guild.iconURL)
+    .setTitle(`${msg.guild.name}`)
+    .addField('**__ Server Name | اسم السيرفر__**',`[** __${msg.guild.name}__ **]`,true)
+    .addField('**__ OwnerShip | الاونر الاساسي__**',`**${msg.guild.owner}**`,true)
+    .addField('**__ Server ID | ايدي السيرفر__**',`**${msg.guild.id}**`,true)
+    .addField('**__ Members Count | عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
+    .addField('**__ Online | الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+    .addField('**__ Verification Level | مستوي الحمايه__**',`[** __${verifLevels[msg.guild.verificationLevel]}__** ]`,true)
+    .addField('**__ Region | البلد__**',`[** __${region[msg.guild.region]}__** ]`,true)
+    .addField('**__ Text Channels | رومات كتابيه__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+    .addField('**__ Voice Channels | رومات صوتيه__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+    .addField('**__ Created At | صنع في __**',msg.guild.createdAt.toLocaleString())
+    msg.channel.send({embed:embed});
+  }
+});
+ 
+
+
+
+
+
+
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
