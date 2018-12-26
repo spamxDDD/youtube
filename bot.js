0985b30ad02abd1f013e3520d22b1c223dfcb449
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-26 17:25:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -242,19 +242,33 @@ client.on("message", (message) => {
 
 
 
-
-client.on("message", message => {
-
-    if (message.content.startsWith(prefix + "bc")) {
-let args = message.content.split(" ").slice(1);
-var argresult = args.join(' '); 
-message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
-m.send(`${argresult}\n ${m}`);
-})
-message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
-message.delete(); 
-};     
-});
-
+  client.on("message", message => {
+    var prefix ="*";
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("**bc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                m.send(`${args}\n ${m}`);
+            });
+            const AziRo = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)   
+            .setTitle('✅| جاري ارسال رسالتك ') 
+            .addBlankField(true)
+            .addField('♨| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
+            .addField('📝| الرسالة ', args)
+            .setColor('RANDOM')  
+            message.channel.sendEmbed(AziRo);          
+        }
+        } else {
+            return;
+        }
+    });
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
