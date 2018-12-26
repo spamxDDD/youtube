0f93b69d4de36d71c80f6eeb4a3dc7e04c162d1d
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-26 11:55:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -269,12 +269,11 @@ client.on('message',async msg => {//Toxic Codes
 });
         client.on('message', async message => {
           if(message.content.startsWith(prefix + "active")) {
-              if(!message.channel.guild) return message.reply message.channel.send("** هذا الامر للسيرفرات فقط :no_entry: ** ");
+      if(!message.channel.guild) return message.reply("** هذا الامر للسيرفرات فقط :no_entry: ** ");
             message.member.addRole(message.guild.roles.find("name". "Active"));
             message.author.send("** تم تفعيلك استمتع :slight_smile: **")
           }
         });
-
 
 
 
