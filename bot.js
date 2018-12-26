e1aca2a0c321544a431868ba9f9e47f80c42b0fe
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-26 16:45:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -245,18 +245,16 @@ client.on("message", (message) => {
 
 client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
+    if (message.content.startsWith(prefix + "bc")) {
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+m.send(`${argresult}\n ${m}`);
 })
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
+message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+message.delete(); 
 };     
 });
-
 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
