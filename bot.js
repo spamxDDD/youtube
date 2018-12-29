4352c67734d24fc28385a4e30a7f7dd75468df27
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-30 00:02:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -271,51 +271,6 @@ client.on("message", (message) => {
         }
     });
 
-const myDiscriminator = ["0000", "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009","1000","2000","3000","4000","5000","6000","7000","8000","9000","0100","0200","0300","0400","0500","0600","0700","0800","0900","0010","0020","0030","0040","0050","0060","0070","0080","0090"];//حطا التاق هناconst bot = new(require("discord.js")).Client({fetchAllMembers: true});
-//Toxic Codes//Toxic Codes//Toxic Codes//Toxic Codes//Toxic Codes//Toxic Codes
-const password = "12344321qwerrewq";//بسورد حسابك عليشن الكود يقدر يغير الاسم
-const token = "NTIwNDUxNzcyNTE5NTQ2ODgw.Dwl4kw.AcyLjuFdgt2UNBqYHM-Trr-AVu0"; // توكن حسابك
-//Toxic Codes
-bot.login(token);//Toxic Codes
-//Toxic Codes
-function changeDiscriminator() {
-  if (myDiscriminator.includes(bot.user.discriminator.toString()))
-    return console.log("Discriminator Loaded: " + bot.user.discriminator);
-  try {//Toxic Codes
-    const us = bot.users.find(u => u.discriminator === bot.user.discriminator && u.username !== bot.user.username && !u.bot).username;
-    console.log(Date.now(), "Username Loaded: " + us);
-    bot.user.setUsername(us, password).then((u) => {
-      console.log(Date.now(), "Username: " + u.username, "Discriminator: " + u.discriminator);
-      setTimeout(changeDiscriminator, 8640 * 10000);
-    }).catch((err) => {//Toxic Codes
-      console.log(Date.now(), "An error occurred. Trying again in sixty (60) seconds.");
-      setTimeout(changeDiscriminator, 60 * 1e3);
-    });//Toxic Codes
-  } catch(e) {//Toxic Codes
-    console.log(Date.now(), [${e}] Trying again in 30 seconds.);
-    setTimeout(changeDiscriminator, 30 * 1e3);
-  }//Toxic Codes
-}//Toxic Codes
- 
-bot.once("ready", () => {//Toxic Codes
-  console.log(Date.now(), "Started with " + bot.users.size + " users.");
-  changeDiscriminator();//Toxic Codes
-  if(myDiscriminator.includes(bot.user.discriminator)) {
-      console.log(I successfully got the discrim ${bot.user.discriminator}!)
-      process.exit();//Toxic Codes
-  }//Toxic Codes
-});//Toxic Codes
-
-
-
-
-
-
-
-
-
-
-
 
 
 
